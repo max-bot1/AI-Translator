8d9a692cdf669bb335c2b021fd5141e566b1447a
maxbot <maxfbotwin@gmail.com>
2024-10-09 23:45:13 -0500

    Hiding this to go public
    This is a secret!

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,5 +5,5 @@ export default defineNuxtConfig({
   modules: ['@nuxt/ui'],
   css: ['~/assets/css/main.css'],
   runtimeConfig: {
-		OPENAI_API_KEY: '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'
+		OPENAI_API_KEY: process.env.OPENAI_API_KEY
 	},})
